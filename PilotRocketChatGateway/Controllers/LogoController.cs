9caf6a2f5f11a613f01b03528a352a5267a639a6
olkhovsky <olkhovskii_av@ascon.ru>
2022-08-05 11:51:24 +0300

    added logo

---
+++ PilotRocketChatGateway/Controllers/LogoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Drawing;
+
+namespace PilotRocketChatGateway.Controllers
+{
+    [ApiController]
+    public class LogoSettingsController : ControllerBase
+    {
+        [HttpGet("/images/logo/android-chrome-512x512.png")]
+        public IActionResult Get()
+        {
+            return File(Properties.Resources.logo, "image/png");
+        }
+    }
+}
