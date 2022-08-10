e8b7799de0b63e29142f0123705b90500444d1a6
olkhovsky <olkhovskii_av@ascon.ru>
2022-08-10 18:38:11 +0300

    added authentication

---
+++ PilotRocketChatGateway/AuthSettings.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace PilotRocketChatGateway
+{
+
+    [Serializable]
+    public class AuthSettings
+    {
+        public string Issuer { get; set; } // издатель токена
+        public string SecretKey { get; set; } // ключ для шифрации
+        public int TokenLifeTimeDays { get; set; } // время жизни токена
+        public int IdleSessionTimeout { get; set; } // время неактивности пользователя. По окончанию этого таймаута сбрасывается подключение к Pilot
+
+        public SymmetricSecurityKey GetSymmetricSecurityKey()
+        {
+            return new SymmetricSecurityKey(Encoding.UTF8.GetBytes(SecretKey));
+        }
+
+        public string GetAudience()
+        {
+            return "PilotRocketChatGateway"; // потребитель токена
+        }
+
+        public TimeSpan GetClockCrew()
+        {
+            return TimeSpan.Zero;
+        }
+    }
+}
--- PilotRocketChatGateway/Program.cs
+++ PilotRocketChatGateway/Program.cs
@@ -1,4 +1,7 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using PilotRocketChatGateway;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,14 +9,46 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.Configure<PilotSettings>(builder.Configuration.GetSection("PilotServer"));
+builder.Services.Configure<AuthSettings>(builder.Configuration.GetSection("AuthSettings"));
 
+var authSettings = builder.Configuration.GetSection("AuthSettings").Get<AuthSettings>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidIssuer = authSettings.Issuer,
+        ValidateAudience = false,
+        ValidAudience = authSettings.GetAudience(),
+        ValidateLifetime = true,
+        IssuerSigningKey = authSettings.GetSymmetricSecurityKey(),
+        ValidateIssuerSigningKey = true,
+        ClockSkew = authSettings.GetClockCrew()
+    };
 
+    options.Events = new JwtBearerEvents
+    {
+        OnMessageReceived = context =>
+        {
+            context.Request.Headers.TryGetValue("x-auth-token", out var token);
+            if (string.IsNullOrEmpty(token))
+            {
+                context.NoResult();
+                return Task.CompletedTask;
+            }
+
+            context.Token = token;
+            return Task.CompletedTask;
+        }
+    };
+});
 
 var app = builder.Build();
 
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 app.UseWebSockets();
 
