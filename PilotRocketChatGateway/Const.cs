f5531bf6b4837a193d5ff0e8dfc1a336619a04b5
olkhovskii_av <olkhovskii_av@ascon.ru>
2024-08-09 11:24:57 +0300

    upped version

--- PilotRocketChatGateway/Const.cs
+++ PilotRocketChatGateway/Const.cs
@@ -2,7 +2,7 @@
 {
     public class Const
     {
-        public const string SERVER_VERSION = "6.6.3";
+        public const string SERVER_VERSION = "6.11.0";
         public const string CLOUD_URI = "https://cloud.rocket.chat";
     }
 }
